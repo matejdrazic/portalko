f8efa977b05cba242b2f7b3be8183c3482c3b2ad
matej <mdrazi01@fesb.hr>
2020-11-05 01:27:47 +0100

    sve vijesti po redu

--- script.js
+++ script.js
@@ -6,11 +6,7 @@ const CORS_PROXY = "https://cors-anywhere.herokuapp.com/";
 //triba učitat prvo index.html inače se neće ovo ispisat
 window.onload = function(){
     var table = document.getElementById("table");
-    var row = table.insertRow(0);
-
-var cell1 = row.insertCell(0);
-var cell2 = row.insertCell(1);
-
+    var counter = 0;
  
 let parser = new RSSParser();
 parser.parseURL(CORS_PROXY + 'https://www.index.hr/rss', function(err, feed) {
@@ -18,12 +14,15 @@ parser.parseURL(CORS_PROXY + 'https://www.index.hr/rss', function(err, feed) {
   console.log(feed.title);
   feed.items.forEach(function(entry) {
     console.log(entry.title + ':' + entry.link);
+    var row = table.insertRow(counter);
+
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
     
     cell1.innerHTML = entry.title;
     cell2.innerHTML = entry.link;
 
-    //Ovo ti pokazuje samo zadnju vijest jer ih stalno mijenja! 
-    //document.body.innerHTML = entry.title + ':' + entry.link; 
+    counter++;
   })
 });
 }
