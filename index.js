42775da1214f2c6dd0f6da79bee2a5f6a90dc040
ibrali01 <ibrali01@fesb.hr>
2020-12-27 21:21:37 +0100

    ponovno popravljeno

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 //triba učitat prvo index.html inače se neće ovo ispisat
-window.onload = async function() {
-  var table = document.getElementById("table");
-  var counter = 0;
-  var cell1;
-  var cell2;
-  var cell3;
-  var cell4;
-  var row;
+window.onload = async function() {              //async - sluzi za obiljezavanje funkcija koje sadrze pozive s await
+  let table = document.getElementById("table");
+  let counter = 0;
+  let cell1;
+  let cell2;
+  let cell3;
+  let cell4;
+  let row;
   
-  var naslov = document.getElementById("naslov");
-  naslov.innerHTML = 'Portalko';
-
-  try {
-    const promise = await hello()
-    promise.forEach(function (arrayItem) {
+  try {                                           // u bloku try navodi se kod koji potencijalno dovodi do iznimke
+    const promise = await hello()                 // await - koristi se za funkcije koje mogu trajati dugo
+    //console.log(promise);
+    promise.forEach(function (arrayItem) {        // ako ne dode do oznimke u bloku try  ne izvodi se kod u bloku catch
       row = table.insertRow(counter);
   
       cell1 = row.insertCell(0);
@@ -22,13 +20,13 @@ window.onload = async function() {
       cell4 = row.insertCell(3);
         
       cell1.innerHTML = arrayItem.title;
-      cell2.innerHTML = arrayItem.guid;
-      cell3.innerHTML = arrayItem.categories;
-      cell1.innerHTML = arrayItem.content;
+      cell2.innerHTML = arrayItem.link;
+      cell3.innerHTML = arrayItem.category;
+      cell4.innerHTML = arrayItem.content;
   
       counter++;
-  });
-  } catch (error) {
+    });
+  } catch (error) {                               //ako dode do iznimke u bloku try, preskače se ostatak bloka try i prelazi u blok catch
     console.log("Error: " + error)
   }
 }
--- parseAndFetch.js
+++ parseAndFetch.js
@@ -1,21 +1,44 @@
 function  hello() {
   const CORS_PROXY = "https://cors-anywhere.herokuapp.com/";
 
+  let articles = [];
+
   let parser = new RSSParser({
     customFields: {
       item: ['content'],
     }
   });
 
-  return new Promise((resolve, reject) => {
-
+  return new Promise((resolve, reject) => {                 //promises su programski entiteti unutar kojih se postavlja posao koji moze potrajati      
+                                                            //posao se obavlja unutar izvršne funkcije, nakon što je posao završen obećanje prelazi u stanje uspjeha ili neuspjeha
     parser.parseURL(CORS_PROXY + 'https://www.index.hr/rss', function (err, feed) {
       if (err)
         throw err;
 
-        if(feed.items.length != 0) resolve(feed.items)
-        else reject("Empty articles")
+      //console.log(feed.items.length);
+      //console.log(feed.items);
+      //console.log(feed.items[5]);
+
+      for(let i = 0; i < feed.items.length; i++) {
+        let article = {
+          "title" : feed.items[i].title,
+          "category" : feed.items[i].categories,
+          "link" : feed.items[i].guid,
+          "content" : feed.items[i].content,
+        }
+        articles.push(article);
+      }
+
+      //console.log(articles);
+
+      if(feed.items.length != 0) 
+        resolve(articles)
+      else 
+        reject("Empty articles")
+    
     });
+  
   })
+
 }
 
