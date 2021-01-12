a44228c258add05665b236e395f7b44d11bcfe9f
ibrali01 <ibrali01@fesb.hr>
2021-01-12 22:03:08 +0100

    10 clanaka

--- public/index.js
+++ public/index.js
@@ -1,7 +1,6 @@
 //triba učitat prvo index.html inače se neće ovo ispisat
 window.onload = async function () {              //async - sluzi za obiljezavanje funkcija koje sadrze pozive s await
   let table = document.getElementById("table");
-  let counter = 0;
   let row;
   let picture;
   let content;
@@ -10,32 +9,16 @@ window.onload = async function () {              //async - sluzi za obiljezavanj
   let guid;
   let category;
 
-  let portali = {
-    Index: 'https://www.index.hr/rss',
-    Telegram: ['https://www.telegram.hr/feed', 'https://telesport.telegram.hr/feed/'],
-    sata24: ['https://www.24sata.hr/feeds/aktualno.xml', 'https://www.24sata.hr/feeds/najnovije.xml', 'https://www.24sata.hr/feeds/news.xml', 'https://www.24sata.hr/feeds/show.xml', 'https://www.24sata.hr/feeds/sport.xml', 'https://www.24sata.hr/feeds/lifestyle.xml', 'https://www.24sata.hr/feeds/tech.xml', 'https://www.24sata.hr/feeds/fun.xml'],
-    DnevnikHr: 'https://dnevnik.hr/assets/feed/articles',
-    Dnevno: 'https://www.dnevno.hr/feed/',
-  }
-
-  let userChoice = [portali.Dnevno, portali.Index];
-  console.log(userChoice);
-  try {                                          
+  try {
     const database = firebase.database();
 
-    // ODI SAN NAPRAVIA DA SE VIJESTI U BAZU ŠALJU SVAKO 10 MINUTI  
-    const promise = await hello(userChoice);                
+    const promise = await hello(database);
 
-    promise.forEach(function (arrayItem) {      
+    //console.log(promise);
+                                                 
+    promise.forEach(function (arrayItem) {
 
-      //console.log(arrayItem.content);
-
-      database.ref('vijesti/' + counter).set({
-        naslov: arrayItem.title,
-        link: arrayItem.link,
-        kategorija: arrayItem.category,
-        kontent: arrayItem.content,
-      });
+      //console.log(arrayItem);
 
       row = document.createElement("div");
       row.classList = "clanak";
@@ -65,37 +48,28 @@ window.onload = async function () {              //async - sluzi za obiljezavanj
       category.id = "kategorija-clanka";
       content.appendChild(category);
 
-
-      var getNews = database.ref('vijesti/' + counter);
-      getNews.on('value', (snapshot) => {
-        var data = snapshot.val();
-        title.innerHTML = data.naslov;
-        category.innerHTML = data.kategorija;
-        guid.innerHTML = data.link;
-        picture.innerHTML = data.kontent;
-        paragraph.innerHTML = picture.textContent;
-      });
-
-      counter++;
+      title.innerHTML = arrayItem.title;
+      category.innerHTML = arrayItem.category;
+      guid.innerHTML = arrayItem.link;
+      picture.innerHTML = arrayItem.content;
+      paragraph.innerHTML = picture.textContent;
     });
-
-  } catch (error) {                               //ako dode do iznimke u bloku try, preskače se ostatak bloka try i prelazi u blok catch
-    console.log("Error: " + error)
+  } catch (error) {
+    console.log("Error: " + error);
   }
 
   document.querySelector("#search-box").addEventListener("keyup", e => {
     let query = e.currentTarget.value;
-    //console.log(query.toUpperCase());
 
     let cards = document.querySelectorAll(".clanak #sadrzaj-clanka");
     for( let card of cards) {
-      //console.log(card.textContent.toUpperCase());
-        if (card.textContent.indexOf(query >= 0)) {
-            card.parentElement.style.display = "block"
-        } else {
-            card.parentElement.style.display = "none";
-        }
+      if (card.textContent.indexOf(query.toUpperCase()) >= 0){
+        card.parentElement.style.display = "block";
+      } else {
+        card.parentElement.style.display = "none";    
+      }
+
     }
-})
+  });
 
 }
--- public/parseAndFetch.js
+++ public/parseAndFetch.js
@@ -1,49 +1,37 @@
-function hello(odabraniPortali) {
-  const CORS_PROXY = "https://cors-anywhere.herokuapp.com/";
+function hello(database) {
 
   let articles = [];
 
-  let broj = 0;
+  return new Promise((resolve, reject) => {                 //promises su programski entiteti unutar kojih se postavlja posao koji moze potrajati   
 
-  let parser = new RSSParser({
-    customFields: {
-      item: ['content'],
-    }
-  });
+    for(let i = 0; i < 10; i++) {
+    
+      var getNews = database.ref('vijesti/' + i);
+      
+      getNews.on('value', (snapshot) => {
+        var data = snapshot.val();
 
-  return new Promise((resolve, reject) => {                 //promises su programski entiteti unutar kojih se postavlja posao koji moze potrajati      
-    for (let i = 0; i < odabraniPortali.length; i++) {
-      //console.log(odabraniPortali[i]);                                                    //posao se obavlja unutar izvršne funkcije, nakon što je posao završen obećanje prelazi u stanje uspjeha ili neuspjeha
-      parser.parseURL(CORS_PROXY + odabraniPortali[i], function (err, feed) {
-
-        if (err)
-          throw err;
-
-        //console.log(feed.items);
-
-        for (let i = 0; i < feed.items.length; i++) {
-          let article = {
-            "title": feed.items[i].title,
-            "category": feed.items[i].categories,
-            "link": feed.items[i].guid,
-            "content": feed.items[i].content,
-          }
-
-          articles.push(article);
+        let article = {
+          "title" : data.naslov,
+          "category" : data.kategorija,
+          "link" :data.link,
+          "content" : data.content,
         }
+  
+        articles.push(article);
 
-        // oni moraju risit izgled stranice, kategorije, i ono da korisnik na početku odabire 'ticka' stranice!
-        // mi sutra moramo filtriranje rišit
-        if (broj == odabraniPortali.length - 1) {
-          if (1 != 0)
-            resolve(articles)
-          else
-            reject("Empty articles")
-        }
-        broj++;
-      })
+      });
+  
     }
-  });
 
+    setTimeout(function(){
+
+      if (articles.length == 10)
+        resolve(articles)
+      else
+        reject("Empty articles")
+    }, 1000)
+    
+  })
 }
 
