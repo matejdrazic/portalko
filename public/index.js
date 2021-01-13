4af1e7884f8f128e620a7aabccfdb958db0b80e3
matejdrazic <mdrazi01@fesb.hr>
2021-01-13 19:09:25 +0100

    Registracija

--- public/index.js
+++ public/index.js
@@ -1,60 +1,74 @@
-//triba učitat prvo index.html inače se neće ovo ispisat
-window.onload = async function () {              //async - sluzi za obiljezavanje funkcija koje sadrze pozive s await
+window.onload = async function () {       
   let table = document.getElementById("table");
   let counter = 0;
-  let cell1;
-  let cell2;
-  let cell3;
-  let cell4;
   let row;
+  let picture;
+  let content;
+  let title;
+  let paragraph;
+  let guid;
+  let category;
 
-  let portali = {
-    Index: 'https://www.index.hr/rss',
-    Telegram: ['https://www.telegram.hr/feed', 'https://telesport.telegram.hr/feed/'],
-    sata24: ['https://www.24sata.hr/feeds/aktualno.xml', 'https://www.24sata.hr/feeds/najnovije.xml', 'https://www.24sata.hr/feeds/news.xml', 'https://www.24sata.hr/feeds/show.xml', 'https://www.24sata.hr/feeds/sport.xml', 'https://www.24sata.hr/feeds/lifestyle.xml', 'https://www.24sata.hr/feeds/tech.xml', 'https://www.24sata.hr/feeds/fun.xml'],
-    DnevnikHr: 'https://dnevnik.hr/assets/feed/articles',
-    Dnevno: 'https://www.dnevno.hr/feed/',
-  }
-
-  let userChoice = [portali.Dnevno, portali.Index];
-  console.log(userChoice);
-  try {                                          
     const database = firebase.database();
+    dohvatDesetClanaka();
+    async function dohvatDesetClanaka() {
+    const promise = await hello(database, counter);
+                                                 
+    promise.forEach(function (arrayItem) {
 
-    // ODI SAN NAPRAVIA DA SE VIJESTI U BAZU ŠALJU SVAKO 10 MINUTI  
-    const promise = await hello(userChoice);                
+      row = document.createElement("div");
+      row.classList = "clanak";
+      table.appendChild(row);
 
-    promise.forEach(function (arrayItem) {      
+      picture = document.createElement("div");
+      picture.id = "slika-clanka";
+      row.appendChild(picture);
 
-      database.ref('vijesti/' + counter).set({
-        naslov: arrayItem.title,
-        link: arrayItem.link,
-        kategorija: arrayItem.category,
-        kontent: arrayItem.content,
-      });
+      content = document.createElement("div");
+      content.id = "sadrzaj-clanka";
+      row.appendChild(content);
 
-      row = table.insertRow(counter);
-      cell1 = row.insertCell(0);
-      cell2 = row.insertCell(1);
-      cell3 = row.insertCell(2);
-      cell4 = row.insertCell(3);
+      title = document.createElement("h2");
+      title.id = "naslov-clanka";
+      content.appendChild(title);
 
-      var getNews = database.ref('vijesti/' + counter);
-      getNews.on('value', (snapshot) => {
-        var data = snapshot.val();
-        cell1.innerHTML = data.naslov;
-        cell2.innerHTML = data.kategorija;
-        cell3.innerHTML = data.link;
-        cell4.innerHTML = data.kontent;
-      });
+      paragraph = document.createElement("p");
+      paragraph.id = "opis-clanka";
+      content.appendChild(paragraph);
 
+      guid = document.createElement("a");
+      guid.id = "link-clanka";
+      content.appendChild(guid);
+
+      category = document.createElement("small");
+      category.id = "kategorija-clanka";
+      content.appendChild(category);
+
+      title.innerHTML = arrayItem.title;
+      category.innerHTML = arrayItem.category;
+      guid.innerHTML = arrayItem.link;
+      picture.innerHTML = arrayItem.content;
+      paragraph.innerHTML = picture.textContent;
       counter++;
     });
+  
+} 
 
-  } catch (error) {                               //ako dode do iznimke u bloku try, preskače se ostatak bloka try i prelazi u blok catch
-    console.log("Error: " + error)
-  }
+  /* document.querySelector("#search-box").addEventListener("keyup", e => {
+    let query = e.currentTarget.value;
 
+    let cards = document.querySelectorAll(".clanak #sadrzaj-clanka");
+    for( let card of cards) {
+      if (card.textContent.toUpperCase().indexOf(query.toUpperCase()) >= 0){
+        card.parentElement.style.display = "block";
+      } else {
+        card.parentElement.style.display = "none";    
+      }
 
+    }
+  }); */
 
-}
+  /* let loadMore = document.querySelector("#ucitaj-jos").addEventListener('click', e => {
+    dohvatDesetClanaka();
+  }); */
+
